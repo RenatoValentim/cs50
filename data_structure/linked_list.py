4926d2830c93f74b8196bbb9382c012576215678
RenatoValentim <rvrenato28@gmail.com>
2026-09-11 16:06:35 -0300

    test(data_structure): add linked list

---
+++ data_structure/linked_list.py
@@ -0,0 +1,113 @@
+from typing import Any, Generator
+
+
+class _Node:
+    def __init__(self, val: Any, next: _Node | None = None) -> None:
+        self.val: Any = val
+        self.next: _Node | None = next
+
+    def __repr__(self) -> str:
+        return f"Node(value: {self.val!r}, next: {self.next!r})"
+
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.__head: _Node | None = None
+        self.__tail: _Node | None = None
+        self.__length: int = 0
+
+    def prepend(self, val: Any) -> None:
+        node = _Node(val)
+        if self.__head is None:
+            self.__head = node
+            self.__tail = node
+            self.__length += 1
+            return
+        temp = self.__head
+        self.__head = node
+        self.__head.next = temp
+        self.__length += 1
+
+    def append(self, val: Any) -> None:
+        node = _Node(val)
+        if self.__head is None:
+            self.__head = node
+            self.__tail = node
+            self.__length += 1
+            return
+        self.__tail.next = node
+        self.__tail = node
+        self.__length += 1
+
+    def find(self, val: Any) -> _Node | None:
+        current = self.__head
+        while current is not None:
+            if current.val == val:
+                return current
+            current = current.next
+        return None
+
+    def remove(self, val: Any) -> bool:
+        if self.__head is None:
+            return False
+        if self.__head.val == val:
+            if self.__head is self.__tail:
+                self.__tail = self.__head.next
+            self.__head = self.__head.next
+            self.__length -= 1
+            return True
+        previous: _Node | None = None
+        current: _Node | None = self.__head
+        while current.next is not None:
+            previous = current
+            current = current.next
+            if current.val == val:
+                if current is self.__tail:
+                    self.__tail = previous
+                previous.next = current.next
+                self.__length -= 1
+                return True
+        return False
+
+    def get(self, index: int) -> _Node | None:
+        current = self.__head
+        i = 0
+        while current is not None:
+            if i == index:
+                return current
+            current = current.next
+            i += 1
+        return None
+
+    def insert_after(self, target_val: Any, new_val: Any) -> bool:
+        if self.__tail is None:
+            return False
+        if target_val == self.__tail.val:
+            self.append(new_val)
+            return True
+        node = _Node(val=new_val)
+        current = self.__head
+        while current is not None:
+            if current.val == target_val:
+                temp = current.next
+                current.next = node
+                node.next = temp
+                return True
+            current = current.next
+        return False
+
+    def reverse(self) -> None: ...
+
+    def __len__(self) -> int:
+        return self.__length
+
+    def __iter__(self) -> Generator[Any, Any, None]:
+        current: _Node | None = self.__head
+        while current is not None:
+            yield current.val
+            current = current.next
+
+    def __repr__(self) -> str:
+        if self.__head is None:
+            return "LinkedList(head: None, tail: None)"
+        return f"LinkedList(head: Node(value: {self.__head.val!r}, next: {self.__head.next!r}), tail: {self.__tail!r})"
---
+++ data_structure/test_linked_list.py
@@ -0,0 +1,170 @@
+import pytest
+from data_structure.linked_list import LinkedList
+
+
+def walk_values(ll: LinkedList) -> list:
+    vals = []
+    for val in ll:
+        vals.append(val)
+    return vals
+
+
+@pytest.mark.parametrize(
+    "values_to_prepend, expected_order",
+    [
+        ([1], [1]),
+        ([1, 2, 3], [3, 2, 1]),
+        ([5, 5, 5], [5, 5, 5]),
+        ([], []),
+    ],
+)
+def test_prepend(values_to_prepend, expected_order):
+    ll = LinkedList()
+    for val in values_to_prepend:
+        ll.prepend(val)
+    assert walk_values(ll) == expected_order
+
+
+@pytest.mark.parametrize(
+    "values_to_append, expected_order",
+    [
+        ([1], [1]),
+        ([1, 2, 3], [1, 2, 3]),
+        ([5, 5, 5], [5, 5, 5]),
+        ([], []),
+    ],
+)
+def test_append(values_to_append, expected_order):
+    ll = LinkedList()
+    for val in values_to_append:
+        ll.append(val)
+    assert walk_values(ll) == expected_order
+
+
+@pytest.mark.parametrize(
+    "initial_values, val_to_find, should_find",
+    [
+        ([1, 2, 3], 2, True),
+        ([1, 2, 3], 1, True),
+        ([1, 2, 3], 3, True),
+        ([1, 2, 3], 99, False),
+        ([], 1, False),
+    ],
+)
+def test_find(initial_values, val_to_find, should_find):
+    ll = LinkedList()
+    for val in initial_values:
+        ll.append(val)
+
+    result = ll.find(val=val_to_find)
+
+    if should_find:
+        assert result is not None
+        assert result.val == val_to_find
+    else:
+        assert result is None
+
+
+@pytest.mark.parametrize(
+    "initial_values, val_to_remove, expected_return, expected_order",
+    [
+        ([1, 2, 3], 2, True, [1, 3]),
+        ([1, 2, 3], 1, True, [2, 3]),
+        ([1, 2, 3], 3, True, [1, 2]),
+        ([1, 2, 3], 99, False, [1, 2, 3]),
+        ([1], 1, True, []),
+        ([], 1, False, []),
+        (
+            [1, 2, 3, 2],
+            2,
+            True,
+            [1, 3, 2],
+        ),
+    ],
+)
+def test_remove(initial_values, val_to_remove, expected_return, expected_order):
+    ll = LinkedList()
+    for val in initial_values:
+        ll.append(val)
+
+    result = ll.remove(val=val_to_remove)
+
+    assert result is expected_return
+    assert walk_values(ll) == expected_order
+
+
+@pytest.mark.parametrize(
+    "initial_values, val_to_remove",
+    [
+        ([1, 2, 3], 3),
+        ([1], 1),
+    ],
+)
+def test_remove_updates_tail_correctly(initial_values, val_to_remove):
+    ll = LinkedList()
+    for val in initial_values:
+        ll.append(val)
+
+    ll.remove(val=val_to_remove)
+    ll.append(999)
+    tail  = ll.get(len(ll)-1)
+
+    assert walk_values(ll)[-1] == 999
+    assert tail is not None
+    assert tail.val == 999
+
+
+@pytest.mark.parametrize(
+    "initial_values, expected_return",
+    [
+        ([], 0),
+        ([1], 1),
+        ([1, 2, 3], 3),
+        ([5, 9], 2),
+    ],
+)
+def test_length(initial_values, expected_return):
+    ll = LinkedList()
+    for val in initial_values:
+        ll.append(val)
+    
+    assert len(ll) == expected_return
+
+    if len(initial_values) == 3:
+        ll.remove(2)
+        assert len(ll) == 2
+
+@pytest.mark.parametrize(
+    "initial_values, index, expected_return",
+    [
+        ([2, 4, 8], 1, 4),
+        ([2, 4, 8], -1, None),
+        ([2, 4, 8], 4, None),
+    ],
+)
+def test_get(initial_values, index, expected_return):
+    ll = LinkedList()
+    for val in initial_values:
+        ll.append(val)
+
+    node = ll.get(index)
+
+    if node is not None:
+        assert node.val == expected_return
+    else:
+        assert node == expected_return
+
+@pytest.mark.parametrize(
+    "initial_values, target_val, new_val, expected_return, expected_order",
+    [
+        ([2, 5, 8], 5 , 7, True, [2, 5, 7, 8]),
+        ([2, 5, 8], 8 , 1, True, [2, 5, 8, 1]),
+    ],
+)
+def test_inser_after(initial_values, target_val, new_val, expected_return, expected_order):
+    ll = LinkedList()
+    for val in initial_values:
+        ll.append(val)
+
+    assert ll.insert_after(target_val, new_val) == expected_return
+    assert walk_values(ll) == expected_order
